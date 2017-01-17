efdc42a4eaa88a0d45cdcb17e0b9a33a5bc888e7
Matthew Finn <m.finn11@nuigalway.ie>
2017-01-17 18:51:14 +0000

    updating db functions

--- include/DBFn.php
+++ include/DBFn.php
@@ -19,13 +19,61 @@
 
 		}
 
-		function saveUser($name, $email, $pw){
+		public function saveUser($fname, $surname, $email, $phno, $pw){
 			$uuid = uniqid('', true);
 			$hash = $this->hashSSHA($pw);
         	$encrypted_pw = $hash["encrypted"];
         	$salt = $hash["salt"];
 
+        	$stmt = $this-> conn -> prepare("INSERT INTO userDetails(unique_id, first_name, surname, email, phone_no, encrypted_password, salt, created_at) VALUES(?,?,?,?,?,?,?,?,NOW())");
+        	$stmt-> bind_param("ssss", $uuid,
+        		$fname, $surname, $email, $phno, $encrypted_pw, $salt);
+        	$result = $stmt->execute();
+        	$stmt->close();
 
+        	if($result){
+	        	$stmt = $this-> conn -> prepare("SELECT * FROM userDetails WHERE email =?"); //SQL Statment object
+	        	$stmt -> bind_param("s", $email);
+	        	$stmt->execute();
+	        	$user = $stmt->get_result()->fetch_assoc();
+	        	return $user;
+
+	        }else{
+	        	return false;
+	        }
+
+
+		}
+
+		public function getUser($em, $pw){
+			$stmt = $this->conn->prepare("SELECT * FROM userDetails WHERE email = ?");
+			$stmt->bind_param("s",$em);
+
+			if($stmt->execute()){
+				$user = $stmt->get_result()->fetch_assoc();
+				$stmt ->close();
+
+				/*
+				 * Running verification on user password
+				*/
+
+				$salt = $user['salt'];
+				$encrypted_pw = $user['encrypted_password'];
+				$hash = $this->chechHash($salt, $pw);
+
+				if($encrypted_pw == $hash){
+					return $user;
+				}
+
+			}else{
+
+				return null;
+			}
+		}
+
+		public function doesUserExist($em){
+
+			
 		}
 
 	}
